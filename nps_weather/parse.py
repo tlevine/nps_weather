6598dda1e87edcbd7ef1a63452a50470d7e2a0f3
Thomas Levine <_@thomaslevine.com>
2015-10-29 09:53:21 -0400

    errors

--- nps_weather/parse.py
+++ nps_weather/parse.py
@@ -2,7 +2,7 @@ import lxml.html
 
 def findapark(response):
     html = lxml.html.fromstring(response.content)
-#   html.xpath('//
+    print(html.xpath('//ul[@class="multiselect-container dropdown-menu"]'))
 
 def weather(response):
     html = lxml.html.fromstring(response.content)
