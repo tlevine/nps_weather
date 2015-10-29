89616fbf7238f145a534215c8efa9bc7105660b3
Thomas Levine <_@thomaslevine.com>
2015-10-29 09:52:14 -0400

    errors

--- nps_weather/download.py
+++ nps_weather/download.py
@@ -1,3 +1,4 @@
 import vlermv
+import requests
 
 get = vlermv.Vlermv.memoize('~/.nps-weather')(requests.get)
--- nps_weather/parse.py
+++ nps_weather/parse.py
@@ -2,6 +2,7 @@ import lxml.html
 
 def findapark(response):
     html = lxml.html.fromstring(response.content)
+#   html.xpath('//
 
 def weather(response):
     html = lxml.html.fromstring(response.content)
