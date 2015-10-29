9a8650d9bcc264ae8a463cf5b3251d2faa8c626b
Thomas Levine <_@thomaslevine.com>
2015-10-29 10:38:17 -0400

    setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(name='nps_weather',
+      author='Thomas Levine',
+      author_email='_@thomaslevine.com',
+      description='Look up typical weather in United States National Park Service parks',
+      url='http://dada.pink/nps_weather/',
+      packages=['nps_weather'],
+      install_requires = [
+          'lxml>=3.3.0',
+          'requests>=2.7.0',
+      ],
+      version='0.0.1',
+      license='AGPL',
+      entry_points = {'console_scripts': ['nps-weather = nps_weather.cli:cli']},
+)
