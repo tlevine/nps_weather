ea42fe87ac29bd43bfa66ce15d22395a2ae70930
Thomas Levine <_@thomaslevine.com>
2015-10-30 10:20:47 -0400

    prettier html

--- nps_weather/cli.py
+++ nps_weather/cli.py
@@ -12,21 +12,20 @@ a = argparse.ArgumentParser('Download NPS weather information')
 a.add_argument('--output', '-o', type = argparse.FileType('w'),
                default = sys.stdout)
 
-HEADER = '''Weather in United States National Parks
-========================================
-These are typical weather conditions for different United States national
-parks, each taken from the park's particular weather webpage. I used the
-`nps_weather package <https://pypi.python.org/pypi/nps_weather>`_
-to generate this file.
-
+HEADER = '''
+<style>a.p { margin-left: 0.5em; background-color: grey; }</style>
+<h1>Weather in United States National Parks</h1>
+<p>
+  These are typical weather conditions for different United States national
+  parks, each taken from the park's particular weather webpage. I used the
+  <a href="https://pypi.python.org/pypi/nps_weather">nps_weather package</a>
+  to generate this file.
+</p>
 '''
 
-PARK_TEMPLATE = '''<a href="#%(id)s" id="%(id)s">%(name)s</a>
-----------------------------------------
+PARK_TEMPLATE = '''<h2 id="%(id)s">%(name)s<a class="p" href="#%(id)s">&#182 </a></h2>
 %(weather)s
-
-[Source](%(url)s)
-
+<p><a href="%(url)s">Source</a></p>
 '''
 
 def cli():
@@ -34,4 +33,5 @@ def cli():
     fp.write(HEADER)
     for park in nps_weather():
         if 'weather' in park:
+            park['weather'] = '<p>' + park['weather'].replace('\n\n', '</p><p>') + '</p>'
             fp.write(PARK_TEMPLATE % park)
